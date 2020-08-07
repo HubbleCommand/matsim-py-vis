2ebe6680fa12f507c4126e2641fd27c48291654e
HubbleCommand <39958198+HubbleCommand@users.noreply.github.com>
2020-08-07 11:39:21 +0200

    Update MATSimPyVis.py

--- MATSimPyVis/MATSimPyVis.py
+++ MATSimPyVis/MATSimPyVis.py
@@ -4,6 +4,7 @@ import matsim
 import pandas as pd
 from collections import defaultdict
 import bokeh
+from bokeh.models import ColumnDataSource
 
 #https://gis.stackexchange.com/questions/223653/cannot-get-plot-in-geopandas-to-produce-a-map-of-the-geodataframe
 #Not in a notebook environment, so need to explicitly use MatPlotLib
@@ -62,8 +63,9 @@ def compareEvents(events1Src, events2Src):
     for event in events1:
         if event['type'] == 'entered link':
             link_counts[event['link']] += 1
+            #link_counts[event['link']] = 0
             eventsCounter += 1
-            if eventsCounter % 1000 == 0:
+            if eventsCounter % 10000 == 0:
                 print("Sim 1 : Got to event : " + str(eventsCounter))
     
     eventsCounter = 0;
@@ -71,8 +73,9 @@ def compareEvents(events1Src, events2Src):
     for event in events2:
         if event['type'] == 'entered link':
             link_counts[event['link']] -= 1
+            #link_counts[event['link']] = 0
             eventsCounter += 1
-            if eventsCounter % 1000 == 0:
+            if eventsCounter % 10000 == 0:
                 print("Sim 2 : Got to event : " + str(eventsCounter))
 
     return link_counts
@@ -81,18 +84,25 @@ def compareEvents(events1Src, events2Src):
 # 1. NETWORK: Read a MATSim network:
 #geo = readNetworkFile('D:/MATSim outputs to keep/equil output 2/output_network.xml.gz')
 geo = readNetworkFile('D:/MATSim outputs to keep/ge 10pct tweaking/ge 1 tweaking/output_network.xml.gz')
+#geo = readNetworkFile('D:/MATSim outputs to keep/ge 25pct .5 factor/output_network.xml.gz')
 
 # 2. EVENTS: Stream through a MATSim event file.
 
 #link_counts = compareEvents('D:/MATSim outputs to keep/equil output 1/output_events.xml.gz', 'D:/MATSim outputs to keep/equil output 2/output_events.xml.gz')
 link_counts = compareEvents('D:/MATSim outputs to keep/ge 10pct tweaking/ge 1 tweaking/output_events.xml.gz', 'D:/MATSim outputs to keep/ge 10pct tweaking/ge 2 tweaking/output_events.xml.gz')
+#link_counts = compareEvents('D:/MATSim outputs to keep/ge 25pct .4 factor/output_events.xml.gz', 'D:/MATSim outputs to keep/ge 25pct .5 factor/output_events.xml.gz')
 
 # convert our link_counts dict to a pandas dataframe, with 'link_id' column as the index and 'count' column with value:
 link_counts = pd.DataFrame.from_dict(link_counts, orient='index', columns=['count']).rename_axis('link_id')
 
 # attach counts to our Geopandas network from above
 volumes = geo.merge(link_counts, on='link_id')
-volumes.plot(column='count', figsize=(10,10), cmap='Wistia') #cmap is colormap
+volumes.crs={'init' :'epsg:4326'}
+volumes.to_file("countries.geojson", driver='GeoJSON')
+
+#volumes.plot(column='count', figsize=(10,10), cmap='Wistia') #cmap is colormap
+#https://matplotlib.org/3.1.0/tutorials/colors/colormaps.html
+volumes.plot(column='count', figsize=(10,10), cmap='RdYlGn')
 
 plt.show()
 print("Should be showing...")
