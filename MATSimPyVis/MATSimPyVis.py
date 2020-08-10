5f6206bca7eea48c43e4ddf960a397b34ef79d8b
HubbleCommand <39958198+HubbleCommand@users.noreply.github.com>
2020-08-10 02:47:54 +0200

    Update MATSimPyVis.py

--- MATSimPyVis/MATSimPyVis.py
+++ MATSimPyVis/MATSimPyVis.py
@@ -59,6 +59,13 @@ def compareEvents(events1Src, events2Src):
     
     eventsCounter = 0
 
+    '''
+    Analysing moring rush hour
+    12h = 43200
+
+    so if event['time'] > 43200 ignore, and can break
+    '''
+
     print("Analysis events 1...")
     for event in events1:
         if event['type'] == 'entered link':
@@ -80,17 +87,29 @@ def compareEvents(events1Src, events2Src):
 
     return link_counts
 
+
+
+
 # -------------------------------------------------------------------
 # 1. NETWORK: Read a MATSim network:
 #geo = readNetworkFile('D:/MATSim outputs to keep/equil output 2/output_network.xml.gz')
-geo = readNetworkFile('D:/MATSim outputs to keep/ge 10pct tweaking/ge 1 tweaking/output_network.xml.gz')
+#geo = readNetworkFile('D:/MATSim outputs to keep/ge 10pct tweaking/ge 1 tweaking/output_network.xml.gz')
 #geo = readNetworkFile('D:/MATSim outputs to keep/ge 25pct .5 factor/output_network.xml.gz')
+#geo = readNetworkFile('D:/MATSim outputs to keep/ge 25pct reserve/output_network.xml.gz')
+geo = readNetworkFile('D:/MATSim outputs to keep/zzz 2nd to last run/output_network.xml.gz')
 
 # 2. EVENTS: Stream through a MATSim event file.
 
 #link_counts = compareEvents('D:/MATSim outputs to keep/equil output 1/output_events.xml.gz', 'D:/MATSim outputs to keep/equil output 2/output_events.xml.gz')
-link_counts = compareEvents('D:/MATSim outputs to keep/ge 10pct tweaking/ge 1 tweaking/output_events.xml.gz', 'D:/MATSim outputs to keep/ge 10pct tweaking/ge 2 tweaking/output_events.xml.gz')
+#link_counts = compareEvents('D:/MATSim outputs to keep/ge 10pct tweaking/ge 1 tweaking/output_events.xml.gz', 'D:/MATSim outputs to keep/ge 10pct tweaking/ge 2 tweaking/output_events.xml.gz')
 #link_counts = compareEvents('D:/MATSim outputs to keep/ge 25pct .4 factor/output_events.xml.gz', 'D:/MATSim outputs to keep/ge 25pct .5 factor/output_events.xml.gz')
+#link_counts = compareEvents('D:/MATSim outputs to keep/ge 25pct reserve/output_events.xml.gz', 'D:/MATSim outputs to keep/ge 25pct no reserve/output_events.xml.gz')
+#link_counts = compareEvents('D:/MATSim outputs to keep/ge 25pct reserve/output_events.xml.gz', 'D:/MATSim outputs to keep/ge 25pct no reserve/output_events.xml.gz')
+link_counts = compareEvents(
+    'D:/MATSim outputs to keep/zzz last run 1.0 factor reserve 0.0/output_events.xml.gz',    #0%
+                            'D:/MATSim outputs to keep/zzz 2nd to last run/output_events.xml.gz'                    #100%
+                            
+                            )
 
 # convert our link_counts dict to a pandas dataframe, with 'link_id' column as the index and 'count' column with value:
 link_counts = pd.DataFrame.from_dict(link_counts, orient='index', columns=['count']).rename_axis('link_id')
