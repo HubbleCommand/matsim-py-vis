9f1034fea78e3db70e2ea4ba3cd7b6380371ad8f
HubbleCommand <39958198+HubbleCommand@users.noreply.github.com>
2020-08-10 13:22:16 +0200

    Calculate Congestion Ratios
    Added function that can calculate congestion ratios, per hour

--- MATSimPyVis/MATSimPyVis.py
+++ MATSimPyVis/MATSimPyVis.py
@@ -87,9 +87,79 @@ def compareEvents(events1Src, events2Src):
 
     return link_counts
 
+def calculateNetworkCapacity():
+    net = readNetworkFile(netSrc)
+    print(net)
+    print(type(net))
 
+    netCounter = 0;
+    totalCapacity = 0;
+    print("Calculating capacity...")
+    for index, row in net.iterrows():
+        totalCapacity += row['capacity']
+        if netCounter % 10000 == 0:
+                print("Got to link : " + str(index) + " with capacity : " + str(row['capacity']))
 
+    print("Total capacity : " + str(totalCapacity))
+    return totalCapacity
 
+def getHourlyCongestionRatio(netSrc, eventsSrc):
+    totalCapacity = 113844500.0 #Don't need to calculate this again
+
+    dictlista = [defaultdict(int) for x in range(24)]
+
+    eventsCounter = 0;
+
+    events = readEventsFile(eventsSrc)
+    for event in events:
+        if event['type'] == 'entered link':
+            #if event['link'] == 83006:
+            slot = int(event['time'] / 3600)
+            q = dictlista[slot]
+            q[event['link']] += 1
+            dictlista[slot] = q
+
+            eventsCounter += 1
+            #if eventsCounter % 10000 == 0:
+                #print("Got to event : " + str(eventsCounter) + " in slot : " + str(slot))
+                
+    timeSlotNumber = 0;
+    for time in dictlista:
+        sumVolume = sum(time.values())
+        print("Time Slot: " + str(timeSlotNumber) + " Total volume: " + str(sumVolume))
+        print("Time Slot: " + str(timeSlotNumber) + " Congestion ratio : " + str(sumVolume/totalCapacity)) 
+        timeSlotNumber += 1;
+
+def getAllCongestionRatios():
+    print("Getting congestion ratio for no reservations")
+    getHourlyCongestionRatio('D:/MATSim outputs to keep/zzz 2nd to last run/output_network.xml.gz', 
+        "D:/MATSim outputs to keep/zzz last run 1.0 factor reserve 0.0/output_events.xml.gz")
+
+    print("Getting congestion ratio for 30% reservations")
+    getHourlyCongestionRatio('D:/MATSim outputs to keep/zzz 2nd to last run/output_network.xml.gz', 
+        "D:/MATSim outputs to keep/ge 25pct 1.0 factor reserve 0.3/output_events.xml.gz")
+
+    print("Getting congestion ratio for 50% reservations")
+    getHourlyCongestionRatio('D:/MATSim outputs to keep/zzz 2nd to last run/output_network.xml.gz', 
+        "D:/MATSim outputs to keep/ge 25pct 1.0 factor reserve 0.5/output_events.xml.gz")
+
+    print("Getting congestion ratio for 60% reservations")
+    getHourlyCongestionRatio('D:/MATSim outputs to keep/zzz 2nd to last run/output_network.xml.gz', 
+        "D:/MATSim outputs to keep/ge 25pct 1.0 factor reserve 0.6/output_events.xml.gz")
+
+    print("Getting congestion ratio for 70% reservations")
+    getHourlyCongestionRatio('D:/MATSim outputs to keep/zzz 2nd to last run/output_network.xml.gz', 
+        "D:/MATSim outputs to keep/ge 25pct 1.0 factor reserve 0.7/output_events.xml.gz")
+
+    print("Getting congestion ratio for 70% reservations real factor")
+    getHourlyCongestionRatio('D:/MATSim outputs to keep/zzz 2nd to last run/output_network.xml.gz', 
+        "D:/MATSim outputs to keep/ge 25pct 1.0 factor reserve 0.7 real factor/output_events.xml.gz")
+
+    print("Getting congestion ratio for 100% reservations")
+    getHourlyCongestionRatio('D:/MATSim outputs to keep/zzz 2nd to last run/output_network.xml.gz', 
+        "D:/MATSim outputs to keep/zzz 2nd to last run/output_events.xml.gz")
+
+'''
 # -------------------------------------------------------------------
 # 1. NETWORK: Read a MATSim network:
 #geo = readNetworkFile('D:/MATSim outputs to keep/equil output 2/output_network.xml.gz')
@@ -105,23 +175,24 @@ geo = readNetworkFile('D:/MATSim outputs to keep/zzz 2nd to last run/output_netw
 #link_counts = compareEvents('D:/MATSim outputs to keep/ge 25pct .4 factor/output_events.xml.gz', 'D:/MATSim outputs to keep/ge 25pct .5 factor/output_events.xml.gz')
 #link_counts = compareEvents('D:/MATSim outputs to keep/ge 25pct reserve/output_events.xml.gz', 'D:/MATSim outputs to keep/ge 25pct no reserve/output_events.xml.gz')
 #link_counts = compareEvents('D:/MATSim outputs to keep/ge 25pct reserve/output_events.xml.gz', 'D:/MATSim outputs to keep/ge 25pct no reserve/output_events.xml.gz')
-link_counts = compareEvents(
-    'D:/MATSim outputs to keep/zzz last run 1.0 factor reserve 0.0/output_events.xml.gz',    #0%
-                            'D:/MATSim outputs to keep/zzz 2nd to last run/output_events.xml.gz'                    #100%
-                            
-                            )
 
-# convert our link_counts dict to a pandas dataframe, with 'link_id' column as the index and 'count' column with value:
-link_counts = pd.DataFrame.from_dict(link_counts, orient='index', columns=['count']).rename_axis('link_id')
+# 0% - 100%
+link_counts = compareEvents("D:/MATSim outputs to keep/zzz last run 1.0 factor reserve 0.0/output_events.xml.gz", "D:/MATSim outputs to keep/zzz 2nd to last run/output_events.xml.gz")
 
-# attach counts to our Geopandas network from above
-volumes = geo.merge(link_counts, on='link_id')
-volumes.crs={'init' :'epsg:4326'}
-volumes.to_file("countries.geojson", driver='GeoJSON')
+ convert our link_counts dict to a pandas dataframe, with "link_id" column as the index and "count" column with value:
+link_counts = pd.DataFrame.from_dict(link_counts, orient="index", columns=["count"]).rename_axis("link_id")
 
-#volumes.plot(column='count', figsize=(10,10), cmap='Wistia') #cmap is colormap
-#https://matplotlib.org/3.1.0/tutorials/colors/colormaps.html
-volumes.plot(column='count', figsize=(10,10), cmap='RdYlGn')
+ attach counts to our Geopandas network from above
+volumes = geo.merge(link_counts, on="link_id")
+volumes.crs={"init" :"epsg:4326"}
+volumes.to_file("countries.geojson", driver="GeoJSON")
+
+ volumes.plot(column="count", figsize=(10,10), cmap="Wistia") #cmap is colormap
+ https://matplotlib.org/3.1.0/tutorials/colors/colormaps.html
+volumes.plot(column="count", figsize=(10,10), cmap="RdYlGn")
 
 plt.show()
 print("Should be showing...")
+'''
+
+getAllCongestionRatios()
